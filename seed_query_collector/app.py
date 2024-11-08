e8185e9bb3a17f065d2213b212c723b25afddd24
Arid Hasan <arid.hasan.h@gmail.com>
2024-11-07 22:36:15 -0400

    updated sqc interface

--- seed_query_collector/app.py
+++ seed_query_collector/app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sentences.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Define countries, locations, languages, and topics
-countries = ['Country1', 'Country2', 'Country3']
-locations = {
-    'Country1': ['Location1', 'Location2'],
-    'Country2': ['Location3', 'Location4'],
-    'Country3': ['Location5', 'Location6']
-}
-languages = ['Language1', 'Language2', 'Language3']
-topics = ['Topic1', 'Topic2', 'Topic3']
+# countries = ['Country1', 'Country2', 'Country3']
+# locations = {
+#     'Country1': ['Location1', 'Location2'],
+#     'Country2': ['Location3', 'Location4'],
+#     'Country3': ['Location5', 'Location6']
+# }
+# languages = ['Language1', 'Language2', 'Language3']
+topics = ['Animals', 'Business', 'Cloths', 'Education', 'Events', 'Food&Drinks', 'General', 'Geography',
+          'Immigration Related', 'Language', 'Literature', 'Names', 'Plants', 'Religion', 'Sports&Games',
+          'Tradition', 'Travel', 'Weather', 'Others']
 
 # Database model for User
 class User(db.Model):
@@ -46,23 +53,33 @@ def index():
         location = request.form.get('location')
         language = request.form.get('language')
         topic = request.form.get('topic')
-        sentence = request.form.get('sentence')
+        #sentence = request.form.get('sentence')
 
-        if sentence:
-            user = User.query.filter_by(email=email).first()
-            if not user:
-                user = User(username=username, email=email)
-                db.session.add(user)
-                db.session.commit()
-
-            new_sentence = Sentence(country=country, location=location, language=language, topic=topic, sentence_text=sentence, user_id=user.id)
-            db.session.add(new_sentence)
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            user = User(username=username, email=email)
+            db.session.add(user)
             db.session.commit()
+
+        if topic == "Others":
+            topic = request.form.get('add_topic')
+        num_sent = int(request.form.get('num_sent'))
+
+        for i in range(1, num_sent+1):
+            sentence = request.form.get(f'sentence_{i}')
+            if sentence:
+                session["user_id"] = user.id
+
+                # data = {'country': country, 'location': location, 'lang': language, 'topic':topic, 'sent': sentence}
+                new_sentence = Sentence(country=country, location=location, language=language, topic=topic, sentence_text=sentence, user_id=user.id)
+                # print(data)
+                db.session.add(new_sentence)
+                db.session.commit()
 
         return redirect(url_for('index'))
 
     sentences = Sentence.query.all()
-    return render_template('index.html', countries=countries, locations=locations, languages=languages, topics=topics, sentences=sentences)
+    return render_template('index.html', topics=topics, sentences=sentences)
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_sentence(id):
@@ -76,6 +93,23 @@ def edit_sentence(id):
         return redirect(url_for('index'))
 
     return render_template('edit.html', sentence=sentence)
+
+@app.route('/view', methods=['GET'])
+def view_sentence():
+    id = None
+    if 'user_id' in session:
+        id = session['user_id']
+    sentences = None
+    if id:
+        sentences = Sentence.query.filter_by(user_id=id).all()
+        # sentences = result.all()
+    return render_template('view.html', sentences=sentences)
+
+@app.route('/contributors', methods=['GET'])
+def contributor():
+    users = User.query.all()
+    return render_template('contributors.html', users=users)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
